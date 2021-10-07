bf9f4f766f04a9dd62c8b08d6662423c66f2b1e3
mstefanutti <matias.stefanutti@mercadolibre.com>
2021-10-06 22:12:27 -0300

    feat: storage with sql added

--- cmd/api/bootstrap/bootstrap.go
+++ cmd/api/bootstrap/bootstrap.go
@@ -1,13 +1,34 @@
 package bootstrap
 
-import "github.com/mstefa/go-api/internal/platform/server"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/mstefa/go-api/internal/platform/server"
+	"github.com/mstefa/go-api/internal/storage/mysql"
+)
 
 const (
 	host = "localhost"
 	port = 8080
+
+	dbUser = "ml_app_user2"
+	dbPass = "ml_app_user2"
+	dbHost = "localhost"
+	dbPort = "3306"
+	dbName = "goapi"
 )
 
 func Run() error {
-	srv := server.New(host, port)
+	mysqlURI := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	db, err := sql.Open("mysql", mysqlURI)
+	if err != nil {
+		return err
+	}
+
+	courseRepository := mysql.NewCourseRepository(db)
+
+	srv := server.New(host, port, courseRepository)
 	return srv.Run()
 }
--- internal/platform/server/server.go
+++ internal/platform/server/server.go
@@ -5,18 +5,25 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
+	mooc "github.com/mstefa/go-api/internal"
+	"github.com/mstefa/go-api/internal/platform/handler/courses"
 	"github.com/mstefa/go-api/internal/platform/handler/health"
 )
 
 type Server struct {
 	httpAddr string
 	engine   *gin.Engine
+
+	// deps
+	courseRepository mooc.CourseRepository
 }
 
-func New(host string, port uint) Server {
+func New(host string, port uint, courseRepository mooc.CourseRepository) Server {
 	srv := Server{
 		httpAddr: fmt.Sprintf("%s:%d", host, port),
 		engine:   gin.New(),
+
+		courseRepository: courseRepository,
 	}
 
 	srv.registerRoutes()
@@ -30,4 +37,5 @@ func (s *Server) Run() error {
 
 func (s *Server) registerRoutes() {
 	s.engine.GET("/health", health.CheckHandler())
+	s.engine.POST("/courses", courses.CreateHandler(s.courseRepository))
 }
